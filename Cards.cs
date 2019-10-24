18059de60cd8c2ba1213a16f6ae75ae02d95a324
Emilind <emilie.lindell@elev.ga.ntig.se>
2019-10-24 16:23:53 +0200

    The right commit?
    Blev något fel, försöker fixa

---
+++ Cards.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novemberprojekt_Emilie_Lindell
+{
+    public enum kortTyp // Jag använder mig av en enumeration för att kunna namnge dessa bestämda konstanter i då en enumeration list. Detta för att även kunna använda det inom denna klass där jag kommer anänvda det.
+    {
+        Hjärter, Klöver, Spader, Ruter
+    }
+    class Cards
+    {
+        public string visaKort { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novemberprojekt_Emilie_Lindell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
