a3c24c19f71675ceac81dfeedd330df2a1d7e78e
Emilind <emilie.lindell@elev.ga.ntig.se>
2019-11-20 14:54:48 +0100

    större omställning
    Det kommer ske stora förändringar, för att efter min research och allt så funka inte vissa saker som ex. "enqueue" osv. så jag beövde ändra metod.

--- Kort.cs
+++ Kort.cs
@@ -4,17 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Novemberprojekt_Emilie_Lindell
 {
     public enum kortTyp // Jag använder mig av en enumeration för att kunna namnge dessa bestämda konstanter i då en enumeration list. Detta för att även kunna använda det inom denna klass där jag kommer anänvda det.
     {
-        Hjärter, Klöver, Spader, Ruter
+        Hearts, Clubs, Spades, Diamonds
+    }
+    public enum värde
+    {
+        TWO = 2, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
+        JACK, QUEEN, KING
+           
+
     }
     class Cards
     {
         public string visaKort { get; set; }//Jag använder det för att kunna på ett kortfattat sätt skriva den typen av egenskap
+        public värde kortVärde { get; set; } // För att sedan kunna visa vilket värde kortet har
         public kortTyp tecken { get; set; } // För att sedan kunna visa vilket tecken kortet har
-        public int värde { get; set; } // För att kunna visa själva numret sen och senare med en koppling till tecknet
 
         public static class mellanting//Jag skapa denna metod för att kunna skapa det som kopplar själva korten och gör koden enklare
         {
@@ -28,6 +36,7 @@ namespace Novemberprojekt_Emilie_Lindell
             }
         }
 
+
         public static class Kortdack // det är i denna metod som jag tänker börja göra själva korten och strukturera upp alla olika korttyper såsom ess, damer och kungar osv. Jag gör den static för att det ska funka samma över alla olika classes.
         {
             public static Queue<Cards> CreateCards()
@@ -35,13 +44,19 @@ namespace Novemberprojekt_Emilie_Lindell
                 Queue<Cards> cards = new Queue<Cards>();
                 for (int i = 2; i <= 14; i++)
                 {
-                    foreach () ;///???? funkar den? Ta reda på!!!!
+                    foreach (kortTyp symbol in enum.getvärde(typeo(kortTyp)))
+                     {}///???? funkar den? Ta reda på!!!!
 
                 }
-                return Shuffle//ta reda på hur man gör shuffle array!!
+                
              }
-
+    public int Count => Cards.Count; 
         }
-
+         public void shuffle()
+{
+    var random = new Random();
+    for (int i = 0; < cards.Count; i++; )
+}    
+         
     }
 }
---
+++ Kortlek.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novemberprojekt_Emilie_Lindell
+{
+    class Kortlek : Kort
+    {
+        const int antalKort = 52; // Jag använder en "const" för att den är konstant och går inte att modify.
+        private Kort[] leken; // detta är då en array som alla kort kommer ligga i
+
+        public Kortlek()
+        {
+            leken = new Kort[antalKort]; // för att registera korten och antelet som en lek.
+
+        }
+
+        public Kort[] getleken {get { return leken; } }// denna metod använder jag för att hämta min kortlek
+
+        public void funkandeKortlek()
+        {
+            int i = 0;
+            foreach(kortTyp)
+        }
+    }
+}
